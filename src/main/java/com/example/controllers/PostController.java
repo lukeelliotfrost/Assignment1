009b9837da59e802228bb9959366128f559da466
lukeelliotfrost <u1450920@unimail.hud.ac.uk>
2017-01-19 16:59:18 +0000

    Added Delete operaton to the PostController

--- src/main/java/com/example/controllers/PostController.java
+++ src/main/java/com/example/controllers/PostController.java
@@ -4,14 +4,16 @@ import com.example.domain.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Luke Frost on 23/12/2016.
  */
 @Controller
-@RequestMapping("/posts")
+@RequestMapping("/reviews")
 public class PostController {
 
     @Autowired
@@ -20,6 +22,18 @@ public class PostController {
     @RequestMapping(value="", method= RequestMethod.GET)
     public String listReviews(Model model) {
         model.addAttribute("reviews", repository.findAll());
-        return "posts/list";
+        return "reviews/list";
     }
+
+    @RequestMapping(value="/{id}/delete", method=RequestMethod.GET)
+    public ModelAndView delete(@PathVariable long id){
+        repository.delete(id);
+        return new ModelAndView("redirect:/posts");
+    }
+
+    
+
+
+
+
 }
